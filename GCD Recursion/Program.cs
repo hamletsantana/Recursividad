752e3076e005e0ce753edaf1629dc698945a277a
Hamlet Santana <danielhamlet123@gmail.com>
2021-06-22 00:58:07 -0400

    Máximo Común Divisor utilizando recursion

---
+++ GCD Recursion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GCD_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Máximo común divisor usando recursión");
+			Console.Write("Inserte un número: ");
+			int numero1 = int.Parse(Console.ReadLine());
+			Console.Write("Inserte otro número: ");
+			int numero2 = int.Parse(Console.ReadLine());
+			
+			Console.WriteLine();
+			Console.WriteLine("El máximo común divisor es: " + GCD(numero1, numero2));
+        }
+		
+		static int GCD(int num1, int num2)
+		{
+			if(num2 == 0)
+			{
+				return num1;
+			}
+			else
+			{
+				return GCD(num2, num1%num2);
+			}
+		}
+    }
+}
