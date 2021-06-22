714fb67c6468a54d420cb58b5878d4862a8d840b
Hamlet Santana <danielhamlet123@gmail.com>
2021-06-21 23:28:25 -0400

    Factorial utilizando recursión

---
+++ Factorial Recursion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factorial_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factorial de un numero usando recursion");
+			Console.Write("Inserte un número: ");
+			int numero = int.Parse(Console.ReadLine());
+			int factorial = Factorial(numero);
+			
+			Console.WriteLine(factorial);
+        }
+		
+		static int Factorial(int num)
+		{
+			if(num > 1)
+			{
+				num = num*Factorial(num-1);
+			}
+			return num;
+			
+		}
+    }
+}
