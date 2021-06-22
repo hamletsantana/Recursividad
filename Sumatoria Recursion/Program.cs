526eeee6e4fbb5d60a21ffb3c56c51810633386c
Hamlet Santana <danielhamlet123@gmail.com>
2021-06-21 23:13:43 -0400

    Sumatoria utilizando recursión

---
+++ Sumatoria Recursion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sumatoria_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sumatoria de un numero usando recursion");
+			Console.Write("Inserte un número: ");
+			int numero = int.Parse(Console.ReadLine());
+			int sumatoria = Sumatoria(numero);
+			
+			Console.WriteLine(sumatoria);
+        }
+		
+		static int Sumatoria(int num)
+		{
+			if(num == 1)
+			{
+				return num;
+			}
+			else
+			{
+				return num + Sumatoria(num-1);
+			}
+		}
+    }
+}
