30cf567044d870840b2fd2c9afab91144d19a606
Hamlet Santana <danielhamlet123@gmail.com>
2021-06-22 00:20:00 -0400

    Sumatoria utilizando iteración

---
+++ Sumatoria Iterativa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sumatoria_Iterativa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sumatoria versión iterativa");
+			Console.Write("Inserte un número: ");
+			int numero = int.Parse(Console.ReadLine());
+			int sumatoria = Sumatoria(numero);
+			
+			Console.WriteLine(sumatoria);
+        }
+		static int Sumatoria(int num)
+		{
+			int sumatoria = 0;
+			for (int i = 0; i <= num; i++)
+			{
+				sumatoria += i; 
+			}
+			return sumatoria;
+		}
+    }
+}
