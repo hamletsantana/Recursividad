af58d361d5e2ecabe01dec47eac9eba41c1fe380
Hamlet Santana <danielhamlet123@gmail.com>
2021-06-22 00:32:43 -0400

    Factorial utilizando iteración

---
+++ Factorial Iterativa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Factorial_Iterativa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factorial versión iterativa");
+			Console.Write("Inserte un número: ");
+			int numero = int.Parse(Console.ReadLine());
+			int factorial = Factorial(numero);
+			
+			Console.WriteLine(factorial);
+        }
+		static int Factorial(int num)
+		{
+			int factorial = 1;
+			for (int i = 1; i <= num; i++)
+			{
+				factorial *= i; 
+			}
+			return factorial;
+		}
+    }
+}
